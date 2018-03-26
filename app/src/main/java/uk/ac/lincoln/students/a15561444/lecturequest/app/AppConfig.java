2d0822c6d647a4ef43f3473cc3ccb149a12f3f59
TomMorley <33935783+TomMorley@users.noreply.github.com>
2018-03-26 21:06:48 +0100

    Initial commit

---
+++ app/src/main/java/uk/ac/lincoln/students/a15561444/lecturequest/app/AppConfig.java
@@ -0,0 +1,13 @@
+package uk.ac.lincoln.students.a15561444.lecturequest.app;
+
+/**
+ * Created by Tom on 26-Mar-18.
+ */
+
+public class AppConfig {
+    public static String URL_LOGIN = "http://thomasmorley.co.uk/task_manager/v1/login";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://thomasmorley.co.uk/task_manager/v1/register";
+
+}
---
+++ app/src/main/java/uk/ac/lincoln/students/a15561444/lecturequest/utils/LruBitmapCache.java
@@ -0,0 +1,43 @@
+package uk.ac.lincoln.students.a15561444.lecturequest.utils;
+
+/**
+ * Created by Tom on 13-Mar-18.
+ */
+
+import com.android.volley.toolbox.ImageLoader.ImageCache;
+
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+public class LruBitmapCache extends LruCache<String, Bitmap> implements
+        ImageCache {
+    public static int getDefaultLruCacheSize() {
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        final int cacheSize = maxMemory / 8;
+
+        return cacheSize;
+    }
+
+    public LruBitmapCache() {
+        this(getDefaultLruCacheSize());
+    }
+
+    public LruBitmapCache(int sizeInKiloBytes) {
+        super(sizeInKiloBytes);
+    }
+
+    @Override
+    protected int sizeOf(String key, Bitmap value) {
+        return value.getRowBytes() * value.getHeight() / 1024;
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return get(url);
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+        put(url, bitmap);
+    }
+}
